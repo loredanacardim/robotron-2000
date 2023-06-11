c7ee71d5ba724b5839eed8d742ec2ffb31936f2a
loredanacardim <loredanacardim@outlook.com>
2023-06-11 17:50:00 +0200

    New update for robot color change

--- js/main.js
+++ js/main.js
@@ -34,6 +34,22 @@ const pecas = {
   }
 }
 
+const botoesCor = document.querySelectorAll('.btn-cor')
+const robo = document.querySelector('.robo')
+
+botoesCor.forEach(botao => {
+  botao.addEventListener('click', () => {
+    console.log('aplicando a cor ' + botao.dataset.cor + ' no Robotron 2000!')
+    robo.src = `img/robotron-${botao.dataset.cor}.png`
+    botao.classList.add('btn-ativo')
+
+    botoesCor.forEach(btn => {
+      if (btn.dataset.cor != botao.dataset.cor)
+        btn.classList.remove('btn-ativo')
+    })
+  })
+})
+
 controle.forEach(elemento => {
   elemento.addEventListener('click', evento => {
     manipulaDados(evento.target.dataset.controle, evento.target.parentNode)
